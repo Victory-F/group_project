68dc0e4492651409a8abaa4c676b5fa12d7e4367
khademishoja <khademishoja@gmail.com>
2023-01-10 16:35:03 +0100

    add gamepage

--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { Game } from "../../../types/gameTypes";
+import { socket } from "../socket/socket";
+
 export const GamePage = () => {
-  return <h2> this is the game page</h2>;
+  const [game, setGame] = useState<Game | null>(null);
+
+  const thisPlayerId = socket.id;
+
+  useEffect(() => {
+    socket.emit("game-playerId", thisPlayerId);
+    socket.on("game", (gameFromServer: Game) => {
+      setGame(gameFromServer);
+    });
+  }, []);
+  return (
+    <div>
+      <h2> this is the game page</h2>
+      <h2>Code: {game?.id}</h2>
+
+      {game?.players.map((p) => (
+        <div>
+          {" "}
+          <div>name:{p.name}</div>
+          <div> score: {p.score} </div>
+          <div>state: {p.state} </div>
+        </div>
+      ))}
+    </div>
+  );
 };
