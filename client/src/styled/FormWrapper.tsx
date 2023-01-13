837a0892eddd621937607eb8f56f5b89b7c70c82
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-01-13 15:08:36 +0100

    Fixed Header

--- client/src/styled/FormWrapper.tsx
+++ client/src/styled/FormWrapper.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 export const FormWrapper = styled.div`
   width: 100%;
   height: 830px;
-  margin-top: -30px;
+  padding-top: 15px;
   background: url("https://static.vecteezy.com/system/resources/thumbnails/001/616/361/original/clip-of-film-reel-and-classic-camera-spinning-with-right-side-light-and-warm-background-in-4k-free-video.jpg");
   background-position: center;
   background-repeat: no-repeat;
